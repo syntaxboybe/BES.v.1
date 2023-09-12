421d26eeba2868e88c15870d3a48de49e499287b
Syntaxboybe <beboymegalbioeyana@gmail.com>
2023-09-13 02:53:57 +0800

    Update
    Update

--- resources/views/notice.blade.php
+++ resources/views/notice.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="bg-light p-5 rounded">
         <h1>Dashboard</h1>
-        
+
         @if (session('resent'))
             <div class="alert alert-success" role="alert">
                 A fresh verification link has been sent to your email address.
@@ -14,7 +14,7 @@
         <form action="{{ route('verification.resend') }}" method="POST" class="d-inline">
             @csrf
             <button type="submit" class="d-inline btn btn-link p-0">
-                click here to request another
+                click here to request another verification
             </button>.
         </form>
     </div>
