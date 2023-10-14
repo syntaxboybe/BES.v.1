bd9112d835b957a78c7b7735d3433c3077384f52
Syntaxboybe <beboymegalbioeyana@gmail.com>
2023-10-14 10:46:46 +0800

    docs: change the image

--- resources/views/pages/ClientSide/userdashboard/homepage.blade.php
+++ resources/views/pages/ClientSide/userdashboard/homepage.blade.php
@@ -28,7 +28,7 @@
                     <div class="carousel-inner">
                         <div class="carousel-item active"><img class="w-100 d-block" src="{{ URL::to('images/bg.jpg') }}" alt="Slide Image"></div>
                         <div class="carousel-item"><img class="w-100 d-block" src="{{ URL::to('images/homepage.jpg') }}" alt="Slide Image"></div>
-                        <div class="carousel-item"><img class="w-100 d-block" src="{{ URL::to('images/map.jpg') }}" alt="Slide Image"></div>
+                        <div class="carousel-item"><img class="w-100 d-block" src="{{ URL::to('images/satellitemap.jpg') }}" alt="Slide Image"></div>
                     </div>
                     <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-slide="prev"><span class="carousel-control-prev-icon"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-slide="next"><span class="carousel-control-next-icon"></span><span class="sr-only">Next</span></a></div>
                     <ol class="carousel-indicators">
