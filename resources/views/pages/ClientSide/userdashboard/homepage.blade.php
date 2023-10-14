3e5dff200efbeaad15699d7afb4c6e3fd372b158
Syntaxboybe <beboymegalbioeyana@gmail.com>
2023-10-14 10:48:09 +0800

    docs: swap image position

--- resources/views/pages/ClientSide/userdashboard/homepage.blade.php
+++ resources/views/pages/ClientSide/userdashboard/homepage.blade.php
@@ -26,8 +26,8 @@
             <div class="col-md-6">
                 <div class="carousel slide" data-ride="carousel" id="carousel-1" style="background-size: cover;">
                     <div class="carousel-inner">
-                        <div class="carousel-item active"><img class="w-100 d-block" src="{{ URL::to('images/bg.jpg') }}" alt="Slide Image"></div>
-                        <div class="carousel-item"><img class="w-100 d-block" src="{{ URL::to('images/homepage.jpg') }}" alt="Slide Image"></div>
+                        <div class="carousel-item active"><img class="w-100 d-block" src="{{ URL::to('images/homepage.jpg') }}" alt="Slide Image"></div>
+                        <div class="carousel-item"><img class="w-100 d-block" src="{{ URL::to('images/bg.jpg') }}" alt="Slide Image"></div>
                         <div class="carousel-item"><img class="w-100 d-block" src="{{ URL::to('images/satellitemap.jpg') }}" alt="Slide Image"></div>
                     </div>
                     <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-slide="prev"><span class="carousel-control-prev-icon"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-slide="next"><span class="carousel-control-next-icon"></span><span class="sr-only">Next</span></a></div>
